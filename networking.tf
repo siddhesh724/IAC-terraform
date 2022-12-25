ea9a1e010d17231bc9e76bfae6ae8ea523d9c65d
siddhesh724 <siddheshtendulkar724@gmail.com>
2022-12-25 20:10:59 +0530

    private RT and Association done

--- networking.tf
+++ networking.tf
@@ -72,3 +72,24 @@ resource "aws_route_table_association" "terraform-public-RT" {
   subnet_id      = aws_subnet.terraform_public_subnet.*.id[count.index]
   route_table_id = aws_route_table.terraform_public_route_table.id
 }
+
+//private route table
+resource "aws_route_table" "terraform_private_route_table" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  /* route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  } */
+
+
+  tags = {
+    Name = "terraform-private-route-table"
+  }
+}
+// Public route table assocition
+resource "aws_route_table_association" "terraform-private-RT" {
+    count = length(var.pub_subnet_cidr)
+  subnet_id      = aws_subnet.terraform_private_subnet.*.id[count.index]
+  route_table_id = aws_route_table.terraform_private_route_table.id
+}
