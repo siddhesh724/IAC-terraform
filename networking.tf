995840ea41da4837bdecc413f3c48651ae7e6040
siddhesh724 <siddheshtendulkar724@gmail.com>
2022-12-25 20:02:54 +0530

    public Rt and route table association done

--- networking.tf
+++ networking.tf
@@ -66,3 +66,9 @@ resource "aws_route_table" "terraform_public_route_table" {
     Name = "terraform-public-route-table"
   }
 }
+// Public route table assocition
+resource "aws_route_table_association" "terraform-public-RT" {
+    count = length(var.pub_subnet_cidr)
+  subnet_id      = aws_subnet.terraform_public_subnet.*.id[count.index]
+  route_table_id = aws_route_table.terraform_public_route_table.id
+}
