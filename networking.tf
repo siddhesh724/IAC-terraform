0ba02f1d44bb96c1c3ac9c496e5a66072e50cc0c
siddhesh724 <siddheshtendulkar724@gmail.com>
2022-12-25 19:22:55 +0530

    public and private subnet is define

--- networking.tf
+++ networking.tf
@@ -1,3 +1,5 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "terraform_vpc" {
   cidr_block           = var.vpc
   instance_tenancy     = "default"
@@ -6,6 +8,36 @@ resource "aws_vpc" "terraform_vpc" {
 
   tags = {
     Name = "terraform_vpc"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+//public subnet
+resource "aws_subnet" "terraform_public_subnet" {
+  count                   = length(var.pub_subnet_cidr)
+  vpc_id                  = aws_vpc.terraform_vpc.id
+  cidr_block              = var.pub_subnet_cidr[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "terraform-public-subnet"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+//private subnet
+resource "aws_subnet" "terraform_private_subnet" {
+  count                   = length(var.private_subnet_cidr)
+  vpc_id                  = aws_vpc.terraform_vpc.id
+  cidr_block              = var.private_subnet_cidr[count.index]
+  map_public_ip_on_launch = false
+  availability_zone       = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "terraform-private-subnet"
   }
   lifecycle {
     create_before_destroy = true
