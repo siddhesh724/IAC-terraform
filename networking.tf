cef2757f2d435fb0b79e1a07156f9e58bb3b6778
siddhesh724 <siddheshtendulkar724@gmail.com>
2022-12-25 19:53:52 +0530

    public RT done

--- networking.tf
+++ networking.tf
@@ -50,4 +50,19 @@ resource "aws_internet_gateway" "igw" {
   tags = {
     Name = "terraform-igw"
   }
+
+}
+//public route table
+resource "aws_route_table" "terraform_public_route_table" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+
+  tags = {
+    Name = "terraform-public-route-table"
+  }
 }
